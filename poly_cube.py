b09e14e22200faeecadcd3e3c3ddce5f03cd95b9
Marta Feriani <marta.celeste.feriani@gmail.com>
2016-10-27 12:08:27 +0100

    poly_cube.py: added getAttr function

--- poly_cube.py
+++ poly_cube.py
@@ -1,2 +1,18 @@
 #importing Python Maya commands
 import maya.cmds as cmds
+
+#create a polygonal cube
+#if not specificated, length and depth are assigned
+#to the default value of 1
+cmds.polyCube(name = 'cube', width = 3)
+
+#getting values of x, y, z translation
+x = cmds.getAttr('cube.tx')
+y = cmds.getAttr('cube.ty')
+z = cmds.getAttr('cube.tz')
+
+#creating a new string interpolated with x, y, z values
+s = "[ %s, %s, %s ]" % (x, y, z)
+
+#printing values
+print s
